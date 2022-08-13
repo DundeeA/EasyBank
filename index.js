c37006baed0d86068fbd6b28c0f138dc52f11f77
dun <DundeeDevelops@gmail.com>
2022-08-12 22:18:56 -0500

    Cosmetic modifications to mobile menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const menuButton = document.getElementById('hamburgerIcon');
+const menuBg = document.getElementById('menuBackground');
 const menu = document.getElementById('menu');
 
 let menuOpen = false;
@@ -7,9 +8,11 @@ menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
     menuButton.style.content = "url('./images/icon-hamburger.svg')"; 
     menu.style.display = 'none';
+    menuBg.style.display = 'none';
    }else{
     menuButton.style.content = "url('./images/icon-close.svg')"; 
     menu.style.display = 'flex';
+    menuBg.style.display = 'block'; 
    }
    menuOpen = !menuOpen;
 })
