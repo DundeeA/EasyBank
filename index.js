259a4ba47a29cb30ed47b4bacde64e110fe750a1
dun <DundeeDevelops@gmail.com>
2022-08-14 19:46:53 -0500

    Mobile menu animation

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ let menuOpen = false;
 menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
     menuButton.style.content = "url('./images/icon-hamburger.svg')"; 
-    menu.style.display = 'none';
-    menuBg.style.display = 'none';
+   
+    menu.style.animation="closeMenu 1s";
+
+    setTimeout(() => {
+       menu.style.display = 'none';
+       menuBg.style.display = 'none';
+    }, 900);
+
+    document.querySelector('html').style.overflow = 'visible';
    }else{
     menuButton.style.content = "url('./images/icon-close.svg')"; 
     menu.style.display = 'flex';
+    menu.style.animation="openMenu 1s";
     menuBg.style.display = 'block'; 
+    document.querySelector('html').style.overflow = 'hidden';
    }
    menuOpen = !menuOpen;
 })
+
