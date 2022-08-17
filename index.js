6b4b6e7e59360680c15b7c50de91b96c7830b74c
dun <DundeeDevelops@gmail.com>
2022-08-17 13:25:51 -0500

    Animation test 0.03

--- index.js
+++ index.js
@@ -7,28 +7,32 @@ let menuOpen = false;
 
 menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
-
     menuIcon.style.content = "url('./images/icon-hamburger.svg')"; //change icon back
-    menu.style.animation="closeMenu .7s"; //play close animation
+
+    //play close animation
+    menu.style.animation="closeMenu .7s"; 
     menuBg.style.animation = 'fadeBgOut .7s ease-in';
+   
     //hide menu
     setTimeout(() => {
        menu.style.display = 'none';
        menuBg.style.display = 'none';
     }, 700);
+    //enable scrolling
+    document.querySelector('html').style.overflow = 'visible'; 
 
-    document.querySelector('html').style.overflow = 'visible'; //enable scrolling
    }else{
     menuIcon.style.content = "url('./images/icon-close.svg')"; //change icon to 'X'
     menu.style.opacity = '0%';
+
     //play open animation
     menu.style.animation="openMenu .7s";
     menuBg.style.animation = 'fadeBgIn .7s ease-in';
+    menu.style.opacity = '100%';
     //Show menu
     setTimeout(() => {
       menu.style.display = 'flex';
       menuBg.style.display = 'block'; 
-      menu.style.opacity = '100%';
    }, 100);
 
 
