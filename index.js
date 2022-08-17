8ef7be10a6b033e73fb6446fad25c9098551434a
dun <DundeeDevelops@gmail.com>
2022-08-17 00:41:08 -0500

    Fixed menu item hover effects

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ let menuOpen = false;
 
 menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
-    menuIcon.style.content = "url('./images/icon-hamburger.svg')"; 
-    menu.style.animation="closeMenu .7s";
+
+    menuIcon.style.content = "url('./images/icon-hamburger.svg')"; //change icon back
+    menu.style.animation="closeMenu .7s"; //play close animation
     menuBg.style.animation = 'fadeBgOut .7s ease-in';
+    //hide menu
     setTimeout(() => {
        menu.style.display = 'none';
        menuBg.style.display = 'none';
     }, 700);
-    document.querySelector('html').style.overflow = 'visible';
+
+    document.querySelector('html').style.overflow = 'visible'; //enable scrolling
    }else{
-    menuIcon.style.content = "url('./images/icon-close.svg')"; 
+    menuIcon.style.content = "url('./images/icon-close.svg')"; //change icon to 'X'
+    //play open animation
     menu.style.animation="openMenu .7s";
     menuBg.style.animation = 'fadeBgIn .7s ease-in';
+    //Show menu
     menu.style.display = 'flex';
     menuBg.style.display = 'block'; 
-    document.querySelector('html').style.overflow = 'hidden';
+    document.querySelector('html').style.overflow = 'hidden'; // disable scrolling
    }
    menuOpen = !menuOpen;
 })
