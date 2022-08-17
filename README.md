8ef38aaee153a9054e0b59207cffa156e71bdfa7
dun <DundeeDevelops@gmail.com>
2022-08-17 17:31:51 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # EasyBank
- Easybank landing page 
+Fictional bank landing page.
 
 
-https://dundeea.github.io/EasyBank/
+### Project challenges
+
+I began this project with nothing more than an image of the design. I took this project on as an opprotunity to practice the fundementals of web development. I wanted to code everything from scratch in order to create a strong foundation on which to continue learning.
+
+### My process
+
+Any good work starts with the preparation. I began work by studying the design image I was provided, taking notes on how I would nest my html elements in order to later make responsive. Once I felt ready
+I got to work developing the sections from top to bottom starting with the navigation and making my way down. 
+
+### Built with
+- HTML
+- CSS
+- JAVASCRIPT
+
+
+- Live solution: [Easybank](https://dundeea.github.io/EasyBank/)
+
+
+### Screenshots 
+ https://i.imgur.com/9h8R33N.png
+ 
+ https://i.imgur.com/u3mriXE.png
--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const menuIcon = document.getElementById('hamburgerIcon');
 const menuButton = document.getElementById('menuButton');
 const menuBg = document.getElementById('menuBackground');
 const menu = document.getElementById('menu');
-
 let menuOpen = false;
 
+//Toggle mobile menu
 menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
     menuIcon.style.content = "url('./images/icon-hamburger.svg')"; //change icon back
-
     //play close animation
     menu.style.animation="closeMenu .7s"; 
     menuBg.style.animation = 'fadeBgOut .7s ease-in';
-   
     //hide menu
     setTimeout(() => {
        menu.style.display = 'none';
@@ -20,24 +18,23 @@ menuButton.addEventListener('click', (e) =>{
     }, 700);
     //enable scrolling
     document.querySelector('html').style.overflow = 'visible'; 
-
    }else{
     menuIcon.style.content = "url('./images/icon-close.svg')"; //change icon to 'X'
     menu.style.opacity = '0%';
-
     //play open animation
     menu.style.animation="openMenu .7s";
     menuBg.style.animation = 'fadeBgIn .7s ease-in';
-    menu.style.opacity = '100%';
     //Show menu
     setTimeout(() => {
       menu.style.display = 'flex';
       menuBg.style.display = 'block'; 
+      menu.style.opacity = '100%';
    }, 100);
-
-
     document.querySelector('html').style.overflow = 'hidden'; // disable scrolling
    }
    menuOpen = !menuOpen;
 })
+
+
+
 
