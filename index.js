15da9d187a629ea099f18e1b1fb216bd5f858d82
dun <DundeeDevelops@gmail.com>
2022-08-16 19:37:45 -0500

    Fixed animation bug

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
     menuIcon.style.content = "url('./images/icon-hamburger.svg')"; 
     menu.style.animation="closeMenu .7s";
-    menuBg.style.animation = 'fadeBgOut .7s';
+    menuBg.style.animation = 'fadeBgOut .7s ease-in';
     setTimeout(() => {
        menu.style.display = 'none';
        menuBg.style.display = 'none';
     }, 700);
     document.querySelector('html').style.overflow = 'visible';
    }else{
-      menuIcon.style.content = "url('./images/icon-close.svg')"; 
-    menu.style.display = 'flex';
+    menuIcon.style.content = "url('./images/icon-close.svg')"; 
     menu.style.animation="openMenu .7s";
-    menuBg.style.animation = 'fadeBgIn .7s';
+    menuBg.style.animation = 'fadeBgIn .7s ease-in';
+    menu.style.display = 'flex';
     menuBg.style.display = 'block'; 
     document.querySelector('html').style.overflow = 'hidden';
    }
