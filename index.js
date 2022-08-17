4e756bd98fab6b0a1fb8d065b9d2f1809c42ee4c
dun <DundeeDevelops@gmail.com>
2022-08-16 19:18:40 -0500

    Made mobile menu easier to open

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const menuButton = document.getElementById('hamburgerIcon');
+const menuIcon = document.getElementById('hamburgerIcon');
+const menuButton = document.getElementById('menuButton');
 const menuBg = document.getElementById('menuBackground');
 const menu = document.getElementById('menu');
 
@@ -6,20 +7,19 @@ let menuOpen = false;
 
 menuButton.addEventListener('click', (e) =>{
    if(menuOpen){
-    menuButton.style.content = "url('./images/icon-hamburger.svg')"; 
-   
-    menu.style.animation="closeMenu 1s";
-
+    menuIcon.style.content = "url('./images/icon-hamburger.svg')"; 
+    menu.style.animation="closeMenu .7s";
+    menuBg.style.animation = 'fadeBgOut .7s';
     setTimeout(() => {
        menu.style.display = 'none';
        menuBg.style.display = 'none';
-    }, 900);
-
+    }, 700);
     document.querySelector('html').style.overflow = 'visible';
    }else{
-    menuButton.style.content = "url('./images/icon-close.svg')"; 
+      menuIcon.style.content = "url('./images/icon-close.svg')"; 
     menu.style.display = 'flex';
-    menu.style.animation="openMenu 1s";
+    menu.style.animation="openMenu .7s";
+    menuBg.style.animation = 'fadeBgIn .7s';
     menuBg.style.display = 'block'; 
     document.querySelector('html').style.overflow = 'hidden';
    }
